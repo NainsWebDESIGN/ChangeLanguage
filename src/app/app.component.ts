de1fedbdfb95f45c07e4ee727600eecb70121bb3
Nains <jason20226@gmail.com>
2021-01-22 15:15:08 +0800

    2021-01-22-新增泰文及日文

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,11 @@ import { Component, OnInit } from '@angular/core';
   providers: [LanguageService]
 })
 export class AppComponent implements OnInit {
-  Lang = true;
   Language = {};
   constructor(public lang: LanguageService,) { }
   changeLang(_Lang: string) {
     this.lang.getLang(_Lang);
   }
   ngOnInit() {
-    this.lang.Lang$.subscribe(el => {
-      this.Lang = el == 'zh-tw' ? false : true
-    })
   }
 }
