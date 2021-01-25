ee1fe5c30678e3b07acb2e726b5c7bb2d2f38bb0
Nains <jason20226@gmail.com>
2021-01-25 14:59:13 +0800

    2021-01-25-整理完成

--- src/app/ts/Ts.Lang.ts
+++ src/app/ts/Ts.Lang.ts
@@ -1,33 +1,14 @@
 export const TSlang = {
     "zh-tw": {
-        哈囉: "哈囉",
-        英雄: "英雄",
-        請按按看語系轉換: "請按按看語系轉換",
-        不然小政會打爆你: "不然小政會打爆你",
         AlertSome: "你成功打印了中文語系"
     },
     "en-us": {
-        哈囉: "Hello",
-        英雄: "Hero",
-        請按按看語系轉換: "Please click to see language switch",
-        不然小政會打爆你: "Otherwise Jacky will blow you up",
         AlertSome: "You successfully printed the English language department"
     },
     "en-ta": {
-        哈囉: "สวัสดี",
-        英雄: "ฮีโร่",
-        請按按看語系轉換: "โปรดคลิกเพื่อดูการเปลี่ยนภาษา",
-        不然小政會打爆你: "มิฉะนั้นแจ็คกี้จะระเบิดคุณ",
         AlertSome: "คุณพิมพ์ภาควิชาภาษาไทยสำเร็จ"
     },
     "en-jp": {
-        哈囉: "こんにちは",
-        英雄: "ヒーロー",
-        請按按看語系轉換: "クリックして言語切り替えをご覧ください",
-        不然小政會打爆你: "そうでなければジャッキーはあなたを爆破します",
         AlertSome: "日本語部門の印刷に成功しました"
     }
-}
-export const TRANSLATE_PIPE = {
-    play: TSlang
 }
